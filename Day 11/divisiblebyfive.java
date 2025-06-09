7e7c64f79a6c4af58ff304d5eff58e2620b0f106
Chinmay9704 <saathwikyenumula@gmail.com>
2025-06-09 12:12:12 +0530

    Remove Day 11 directory

--- Day 11/divisiblebyfive.java
+++
@@ -1,7 +0,0 @@
-public class divisiblebyfive {
-    public static void main(String[] args) {
-        for (int i = 5; i <= 100; i += 5) {
-            System.out.print(i + " ");
-        }
-    }
-}
--- Day 11/printoddnumbers.java
+++
@@ -1,7 +0,0 @@
-public class printoddnumbers {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 50; i += 2) {
-            System.out.print(i + " ");
-        }
-    }
-}
